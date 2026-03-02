9a3ec49a03f758ea8677629b967036984b115037
d.h.bradshaw <dhbradshaw@gmail.com>
2026-03-02 11:26:57 -0600

    stub out page

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,41 @@ fn main() {
     launch(App);
 }
 
+#[derive(Routable, Clone, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/fft-draw")]
+    FftDraw {},
+}
+
 #[component]
 fn App() -> Element {
     rsx! {
         document::Title { "Diaudio" }
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    rsx! {
         h1 { "Diaudio" }
+        nav {
+            ul {
+                li {
+                    Link { to: Route::FftDraw {}, "FFT Draw" }
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn FftDraw() -> Element {
+    rsx! {
+        h1 { "FFT Draw" }
+        p { "FFT Draw page stub." }
+        Link { to: Route::Home {}, "Back to Home" }
     }
 }
